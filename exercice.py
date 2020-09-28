ae737d8ac8457a308cd50dd45873bbbd29a48a1a
marieclairetache <marie-claire.tache@polymtl.ca>
2020-09-28 18:01:54 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,7 +12,7 @@ def order(values: list = None) -> bool:
 
 def anagrams(words: list = None) -> bool:
     if words is None:
-        # TODO: Demander les mots ici
+        print
         pass
 
     return False
