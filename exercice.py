bd039b6dbd26b56a4710d2ca905ee74d7197fa2d
marieclairetache <marie-claire.tache@polymtl.ca>
2020-09-28 17:28:34 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@
 def order(values: list = None) -> bool:
     if values is None:
         # TODO: Demander les valeurs ici
-        values = [input("entrez...")]
+        values = [input("entrez...")for n in range (10)]
         list.sort()
         pass
 
